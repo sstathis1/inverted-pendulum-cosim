d80e62e52d14f804d804c9559430b089d1ee1d2d
StevennStathis <stefanosdims@gmail.com>
2022-05-05 00:05:03 +0300

    Changed test script for Master class

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from master import MasterOptions
+from master import MasterOptions, Master
 
-test = MasterOptions(step_size = 0.001, order = 2, rtol = 1e-2, communication_method = "Gauss")
-options = test.get_options()
+master = Master(step_size = 0.001, order = 2, rtol = 1e-2, communication_method = "Gauss")
+options = master.options
 
 for key in options:
     print(key, " : ", options[key])
