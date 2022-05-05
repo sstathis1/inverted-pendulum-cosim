631bec98099141f46aaadfadcc9fab58c278da86
sstathis1 <stefanosdims@gmail.com>
2022-05-05 14:06:05 +0300

    Added print

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from master import MasterOptions
 
-test = MasterOptions(step_size = 0.001, order = 2, rtol = 1e-2, communication_method = "Gauss")
+test = MasterOptions(step_size = 1, order = 2, rtol = 1e-2, communication_method = "Gauss")
 options = test.get_options()
 
 for key in options:
