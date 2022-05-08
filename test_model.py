d8cb4bb3ab3c544372e38763e1c5ddc2d52774a4
StevennStathis <stefanosdims@gmail.com>
2022-05-08 21:45:12 +0300

    Added functionality to test the input, output, simulation methods that were added

--- test_model.py
+++ test_model.py
@@ -1,13 +1,40 @@
 from oscilator import Oscilator
-from math import cos
+from math import cos, sin
+import matplotlib.pyplot as plt
 
-model_1 = Oscilator(1, 100, 1)
-m = model_1.get("m")
-k = model_1.get("k")
-c = model_1.get("c")
-
+# Input function to the model
 def f(t):
-    return 10 * cos(t)
+    return [1 * cos(0.01 * t), -1 * sin(0.01 * t)]
 
-model_1.set_input(("force", f))
-print(model_1.get_inputs(5))
+def main():    
+    model_1 = Oscilator(1, 100, 1)
+    m = model_1.get("m")
+    k = model_1.get("k")
+    c = model_1.get("c")
+
+    print(f"The states of the model before the simulation are: {model_1.states}")
+    print(f"The outputs of the model before the simulation are: {model_1.output}")
+    print(f"The time variable for the model before the simulation is set to be: {model_1.time}")
+
+    # Simulate the model on it's own
+    res = model_1.simulate([0, 1], f, 10)
+
+    print(f"The states of the model after the simulation are: {model_1.states}")
+    print(f"The outputs of the model after the simulation are: {model_1.output}")
+    print(f"The time variable for the model after the simulation is set to be: {model_1.time}")
+    print(f"The input to the model at the end of the simulation is: {model_1.input}")
+
+    # Plot the resuls
+    plt.figure(figsize=[6, 4], dpi=200)
+    plt.plot(res["time"], res["x"], label="x")
+    plt.plot(res["time"], res["v"], label="v")
+    plt.ylabel("x (m), v (m/s)")
+    plt.xlabel("time (s)")
+    plt.legend()
+    plt.xlim(0, res["time"][-1])
+    plt.title("One-Degree of Freedom Linear Oscilator response")
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
