521fb4c84dd796e948d624dd81c2211da5993d8b
sstathis1 <stefanosdims@gmail.com>
2022-05-09 16:31:42 +0300

    Layed the calling sequence for co-sim

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 from master import MasterOptions, Master
+from oscilator import Oscilator
 
-master = Master(step_size = 0.001, order = 2, rtol = 1e-2, communication_method = "Gauss")
-options = master.options
+# Create the two model objects
+model_1 = Oscilator(1, 100, 1, osc_method="displacement")
+model_2 = Oscilator(1, 100, 1, osc_method="displacement")
+models = [model_1, model_2]
 
-for key in options:
-    print(key, " : ", options[key])
+# Define the connections of the models
+connections = [(model_1, "x", model_2, "x_c"), (model_1, "v", model_2, "v_c"), 
+               (model_2, "x", model_1, "x_c"), (model_2, "v", model_1, "v_c")]
+
+# Define the master object for the co-simulation
+master = Master(models, connections, step_size=1e-3, order=1, communication_method="Gauss")
+
+# Simulate the models
+res = master.simulate()
