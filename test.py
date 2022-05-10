b2f024998320fd8481e8831668e510fe92cf5fb0
sstathis1 <stefanosdims@gmail.com>
2022-05-10 19:14:02 +0300

    Deleted connections variable

--- test.py
+++ test.py
@@ -6,12 +6,14 @@ model_1 = Oscilator(1, 100, 1, osc_method="displacement")
 model_2 = Oscilator(1, 100, 1, osc_method="displacement")
 models = [model_1, model_2]
 
-# Define the connections of the models
-connections = [(model_1, "x", model_2, "x_c"), (model_1, "v", model_2, "v_c"), 
-               (model_2, "x", model_1, "x_c"), (model_2, "v", model_1, "v_c")]
+# Define connections
+connections = []
 
 # Define the master object for the co-simulation
-master = Master(models, connections, step_size=1e-3, order=1, communication_method="Gauss")
+master = Master(models, step_size=1e-3, order=1, communication_method="Gauss")
 
 # Simulate the models
-res = master.simulate()
+start_time = 0
+final_time = 12
+initial_states = [0, 1, 1, 0]
+res = master.simulate(initial_states, start_time, final_time)
