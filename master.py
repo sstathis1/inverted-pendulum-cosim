7adb5620641beb3b47da46581238a4278ec3873c
sstathis1 <stefanosdims@gmail.com>
2022-05-10 19:13:06 +0300

    Added methods for simulating, initializing, geting and setting values

--- master.py
+++ master.py
@@ -98,17 +98,87 @@ class Master(MasterOptions):
     Master algorithm for explicit co-simulation of slave models.
     """
 
-    def __init__(self, models, connections, **kw):
+    def __init__(self, models, **kw):
+        super().__init__()
         self.options = kw
         self.models = models
-        self.connections = connections
+        self.check_names()
 
-    def initialize(self, models):
-        for model in models:
-            self.outputs[model.name] = model.output
-            self.states[model.name] = model.states
-            self.inputs[model.name] = model.input
+    def check_names(self):
+        if self.models[0].name == self.models[1].name:
+            self.models[0].name = self.models[0].name + "_0"
+            self.models[1].name = self.models[1].name + "_1"
 
-    def simulate(self, **kw):
-        # Stores the options for the co-simulation or uses default if no keyword arguments are given
+    def initialize(self, states):
+        self.set_states(states)
+
+    def get_outputs(self):
+        output = []
+        for model in self.models:
+            output += list(model.output.values())
+        return output
+
+    def get_states(self):
+        states = []
+        for model in self.models:
+            states += list(model.states.values())
+        return states
+
+    def set_states(self, states):
+        i = 0
+        for model in self.models:
+            model.states = states[i:len(model.states)]
+            i += len(model.states)
+
+    def set_inputs(self, last_inputs):
+        for model in self.models:
+            model.input = self.extrapolate_input(last_inputs)
+
+    def extrapolate_input(self, last_inputs):
+        self._inputs += last_inputs
+
+    def jacobi_algorithm(self, start_time, final_time):
+        step_size = self.options["step_size"]
+        if self.options["error_controlled"]:
+            current_time = start_time
+            while current_time < final_time:
+                # Set states
+                states = self.get_states()
+                
+                self.set_inputs()
+
+                # Take a full step
+                self.perform_step()
+                y_full = self.get_outputs()
+
+                # Restore states
+                self.set_states(states)
+
+                # Take a half step
+                step_size = step_size / 2
+                self.perform_step()
+
+                # Take another half step
+                self.perform_step()
+                y_half = self.get_outputs()
+
+                # Estimate error
+                error = self.estimate_error(y_full, y_half)
+
+                # Calculate next step size
+                self.adapt_stepsize(error)
+        else:
+            pass
+
+    def gauss_algorithm(self, start_time, final_time):
+        pass
+
+    def simulate(self, initial_states, start_time, final_time, **kw):
         self.options = kw
+        self.initialize(initial_states)
+        if self.options["communication_method"] == "Jacobi":
+            self.jacobi_algorithm(start_time, final_time)
+        elif self.options["communication_method"] == "Gauss":
+            self.gauss_algorithm(start_time, final_time)
+        else:
+            raise Exception("A non valid communication method was passed")
