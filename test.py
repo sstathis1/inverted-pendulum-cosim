9940476d04ee11431332fbcbfa7cb7482eb28552
sstathis1 <stefanosdims@gmail.com>
2022-05-11 16:50:11 +0300

    Removed connections variable

--- test.py
+++ test.py
@@ -2,12 +2,9 @@ from master import MasterOptions, Master
 from oscilator import Oscilator
 
 # Create the two model objects
-model_1 = Oscilator(1, 100, 1, osc_method="displacement")
-model_2 = Oscilator(1, 100, 1, osc_method="displacement")
+model_1 = Oscilator(1, 100, 1, osc_method="displacement", output="force")
+model_2 = Oscilator(1, 100, 1, osc_method="displacement", output="displacement")
 models = [model_1, model_2]
-
-# Define connections
-connections = []
 
 # Define the master object for the co-simulation
 master = Master(models, step_size=1e-3, order=1, communication_method="Gauss")
