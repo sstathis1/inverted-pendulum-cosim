10311b8f5611f26bf1de000f2996ec91ff22de35
sstathis1 <stefanosdims@gmail.com>
2022-05-18 17:02:30 +0300

    Added call to animate and improved the test

--- test_model.py
+++ test_model.py
@@ -1,31 +1,44 @@
 from single_pendulum import SinglePendulum
-from numpy import cos, sin
+from numpy import cos, sin 
+from math import pi
 import matplotlib.pyplot as plt
 
-# Input function to the model
-def f(t):
-    return [1 * cos(0.01 * t), -1 * sin(0.01 * t)]
+# Optional: Input function to the model
+def f(x):
+    K = []
+    return - K.dot(x)
 
 def main():    
-    model_1 = SinglePendulum(1, 0.4, 0.5, 0.1)
+    model_1 = SinglePendulum(1, 0.4, 0.4)
     mp = model_1.get("mass_pendulum")
     mc = model_1.get("mass_cart")
     l = model_1.get("length_pendulum")
     I = model_1.get("inertia_pendulum")
-    print(mp, mc, l, I)
+    print(f"mass pendulum: {mp}, mass cart: {mc}, length pendulum: {l}, inertia pendulum: {I}")
 
     # Simulate the model on it's own
-    res = model_1.simulate([0, 0, 0.5235, 0], 20, method="BDF")
+    res = model_1.simulate([0, 0, 10 * pi / 180, 0], 5)
 
-    # Plot the resuls
+    # Create an animation of the results
+    model_1.animate(res, savefig=False)
+
+    # Plot the angle response
     plt.figure(figsize=[6, 4], dpi=200)
-    plt.plot(res["time"], res["x"], label="x (m)")
-    plt.plot(res["time"], res["theta"], label="theta (rad)")
-    plt.ylabel("states")
+    plt.plot(res["time"], res["theta"] * 180 / pi)
+    plt.ylabel("theta (deg)")
     plt.xlabel("time (s)")
-    plt.legend()
     plt.xlim(0, res["time"][-1])
-    plt.title("Single Pendulum on Cart state response")
+    plt.title("Single Pendulum on Cart angle response")
+    plt.grid()
+    plt.show()
+
+    # Plot the position response
+    plt.figure(figsize=[6, 4], dpi=200)
+    plt.plot(res["time"], res["x"])
+    plt.ylabel("x (m)")
+    plt.xlabel("time (s)")
+    plt.xlim(0, res["time"][-1])
+    plt.title("Single Pendulum on Cart position response")
     plt.grid()
     plt.show()
 
